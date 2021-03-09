5609a4a5bb4ab0c7398f018cbb3bebba6546acf9
libin <807242867@qq.com>
2021-03-09 17:55:59 +0800

    引入taro-ui、面积tag

--- src/pages/order_g/index.jsx
+++ src/pages/order_g/index.jsx
@@ -4,14 +4,24 @@ import { observer, inject } from 'mobx-react'
 import Taro from '@tarojs/taro'
 import api from '../../constant/apis'
 import req from '../../utils/request'
+import { AtTag } from 'taro-ui'
 import './index.less'
 
 
 @inject('store')
 @observer
 class Index extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      areaList: [
+        { area: '50-70', choose: false, dosage: 2 }, { area: '70-90', choose: false, dosage: 3 }, { area: '90-110', choose: false, dosage: 3.5 }, { area: '110-130', choose: false, dosage: 4 }, { area: '130-150', choose: false, dosage: 5 },
+      ],
+      dosage:0,
+    }
+  }
 
-  async componentDidMount () {
+  async componentDidMount() {
     // try {
     //   const res = await req.post('/getDataList',{aa:'b'})
     //   console.log(res.data)
@@ -26,13 +36,48 @@ class Index extends Component {
     // }
   }
 
-  
+  chooseArea(index, value) {
+    console.log(index, value);
+    const list = this.state.areaList
+    for (let i = 0; i < 5; i++) {
+      list[i].choose = false
+    }
+    list[index].choose = true
+    this.setState({
+      areaList: list,
+      dosage:list[index].dosage
+    })
+  }
 
-  render () {
+  render() {
     // const { counterStore: { counter } } = this.props.store
     return (
       <View className='g-index'>
-         国标
+        <View className='area'>
+          <View>面积</View>
+          <View className='area-tags'>
+            {
+              this.state.areaList.map((item, index) => {
+                return <AtTag name={item.area} size='small' type='primary' active={item.choose} onClick={this.chooseArea.bind(this, index)}
+                >
+                  {item.area}
+                </AtTag>
+              })
+            }
+          </View>
+          <View className='use'>
+            用量图片{this.state.dosage}
+          </View>
+        </View>
+        <View className='serve'>
+
+        </View>
+        <View className='insurance'>
+
+        </View>
+        <View className='sum'>
+
+        </View>
       </View>
     )
   }
